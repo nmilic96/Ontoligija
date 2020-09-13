5997f61209ef1c875a513436eb7ccb111e99996a
nmili96 <milic.n96@gmail.com>
2020-09-13 14:19:08 +0200

    Fix imports

--- app/src/main/java/ffos/p3/Milic/Adapter.java
+++ app/src/main/java/ffos/p3/Milic/Adapter.java
@@ -9,7 +9,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.ontoligija.R;
+import ffos.p3.Milic.R;
 
 import java.util.ArrayList;
 
--- app/src/main/java/ffos/p3/Milic/MainActivity.java
+++ app/src/main/java/ffos/p3/Milic/MainActivity.java
@@ -17,7 +17,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.ontoligija.R;
+import ffos.p3.Milic.R;
 
 import org.json.JSONArray;
 import org.json.JSONException;
